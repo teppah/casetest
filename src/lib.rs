1aaaa2a16cc8c4c04b437b606fad0fd6094c5d07
Yi Feng Yang <yifeng@yfyang.dev>
2020-09-29 22:40:15 -0400

    Remove duplicate code

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@ use std::io::Write;
 use std::process::{Command, Output, Stdio};
 use std::time::Instant;
 
-use ansi_term::Colour::{Green, Red, White};
 use clap::ArgMatches;
 
 pub fn compile(source_file: &str, output_file: &str) -> std::io::Result<Output> {
@@ -44,11 +43,7 @@ pub fn execute_test_cases(compiled_file: &str, mut lines: core::str::Lines) -> T
         {
             Ok(p) => p,
             Err(e) => {
-                println!("{} Run #{} {}: {}",
-                         Red.paint("✗"),
-                         i,
-                         Red.bold().underline().paint("failed to execute"),
-                         White.dimmed().paint(format!("{}", e)));
+                print::print_exec_fail(i, &e.to_string());
                 failed += 1;
                 continue;
             }
@@ -61,11 +56,7 @@ pub fn execute_test_cases(compiled_file: &str, mut lines: core::str::Lines) -> T
             let stdin = match exec.stdin.as_mut() {
                 Some(stdin) => stdin,
                 None => {
-                    println!("{} Run #{} {}: {}",
-                             Red.paint("✗"),
-                             i,
-                             Red.bold().underline().paint("failed to execute"),
-                             White.dimmed().paint("failed to open stdin"));
+                    print::print_exec_fail(i, "failed to open stdin");
                     failed += 1;
                     continue;
                 }
@@ -73,11 +64,7 @@ pub fn execute_test_cases(compiled_file: &str, mut lines: core::str::Lines) -> T
             match stdin.write_all(test_input.as_bytes()) {
                 Ok(_) => (),
                 Err(e) => {
-                    println!("{} Run #{} {}: {}",
-                             Red.paint("✗"),
-                             i,
-                             Red.bold().underline().paint("failed to execute"),
-                             White.dimmed().paint(format!("{}", e)));
+                    print::print_exec_fail(i, &e.to_string());
                     failed += 1;
                     continue;
                 }
@@ -99,43 +86,66 @@ pub fn execute_test_cases(compiled_file: &str, mut lines: core::str::Lines) -> T
             }
         };
 
-        let mut msg = String::new();
         match is_success {
             true => {
-                msg.push_str(&Green.paint("✔ ").to_string());
-                msg.push_str(&format!(" Run #{} {} ", i, Green.paint("success")));
-                msg.push_str(
-                    &White.dimmed().paint(
-                        &format!("(input: {})", test_input))
-                        .to_string());
+                print::print_success(i, test_input);
                 successful += 1;
             }
             false => {
-                msg.push_str(&Red.paint("✗ ").to_string());
-                msg.push_str(&format!(" Run #{} {} ",
-                                      i,
-                                      Red.bold().underline().paint("failed")));
-                msg.push_str(&format!("{}\n",
-                                      &White.dimmed().paint(
-                                          &format!(" (input: {})", test_input))
-                                          .to_string()));
-                msg.push('\n');
-                msg.push_str(&format!("\t{}:    {}\n",
-                                      &White.dimmed().paint("Expected"),
-                                      expected_output));
-                msg.push_str(&format!("\t{}:         {}",
-                                      &White.dimmed().paint("Got").to_string(),
-                                      output));
-                msg.push('\n');
+                print::print_test_fail(i, test_input, expected_output, &output);
                 failed += 1;
             }
         };
-        println!("{}", msg);
     }
     let total_time = before.elapsed().as_millis();
 
     TestResult { passed: successful, failed, total_time_ms: total_time }
 }
+
+mod print {
+    use ansi_term::Colour::{Green, Red, White};
+
+    pub(crate) fn print_success(run_no: usize, input: &str) {
+        let mut msg = String::new();
+        msg.push_str(&Green.paint("✔ ").to_string());
+        msg.push_str(&format!(" Run #{} {} ", run_no, Green.paint("success")));
+        msg.push_str(
+            &White.dimmed().paint(
+                &format!("(input: {})", input))
+                .to_string());
+        println!("{}", msg);
+    }
+
+    pub(crate) fn print_test_fail(run_no: usize, input: &str, expected: &str, got: &str) {
+        let mut msg = String::new();
+        msg.push_str(&Red.paint("✗ ").to_string());
+        msg.push_str(&format!(" Run #{} {} ",
+                              run_no,
+                              Red.bold().underline().paint("failed")));
+        msg.push_str(&format!("{}\n",
+                              &White.dimmed().paint(
+                                  &format!(" (input: {})", input))
+                                  .to_string()));
+        msg.push('\n');
+        msg.push_str(&format!("\t{}:    {}\n",
+                              &White.dimmed().paint("Expected"),
+                              expected));
+        msg.push_str(&format!("\t{}:         {}",
+                              &White.dimmed().paint("Got").to_string(),
+                              got));
+        msg.push('\n');
+        println!("{}", msg);
+    }
+
+    pub(crate) fn print_exec_fail(run_no: usize, err_str: &str) {
+        println!("{} Run #{} {}: {}",
+                 Red.paint("✗"),
+                 run_no,
+                 Red.bold().underline().paint("failed to execute"),
+                 White.dimmed().paint(err_str));
+    }
+}
+
 
 pub struct TestResult {
     pub passed: u32,
