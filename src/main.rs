202715be4b7e8e10918081cfd22572fc3f154332
Yi Feng Yang <yifeng@yfyang.dev>
2020-10-26 22:21:27 -0400

    Make watch and test subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::fs;
 
 use ansi_term::Colour::{Blue, Green, Red};
-use clap::{App, Arg};
+use clap::{App, Arg, SubCommand, AppSettings};
 
 use casetest::{compile, execute_test_cases, FileNames, get_files, TestResult};
 
@@ -10,50 +10,64 @@ fn main() {
         .version("1.0")
         .author("Yi Feng Yang <yifeng@yfyang.dev>")
         .about("A tool to run unit tests against single-file C programs")
-        .arg(Arg::with_name("file")
-            .value_name("PROGRAM")
-            .help("The file to run test cases against")
-            .index(1)
-            .required(true))
-        .arg(Arg::with_name("cases")
-            .value_name("TESTS")
-            .help("A plaintext file with test cases on odd lines and expected output on even lines")
-            .index(2)
-            .required(true));
-
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .subcommand(
+            SubCommand::with_name("test")
+                .about("Run unit tests on a C file with a given input file")
+                .arg(Arg::with_name("file")
+                    .value_name("PROGRAM")
+                    .help("The file to run test cases against")
+                    .index(1)
+                    .required(true))
+                .arg(Arg::with_name("cases")
+                    .value_name("TESTS")
+                    .help("A plaintext file with test cases on odd lines and expected output on even lines")
+                    .index(2)
+                    .required(true)))
+        .subcommand(
+            SubCommand::with_name("watch")
+        );
     let matches = app.get_matches();
-    let FileNames { c_file, test_file, compiled_file } = get_files(&matches);
+    println!("{:?}", matches);
 
-    match compile(&c_file, &compiled_file) {
-        Ok(out) => {
-            if !out.status.success() {
-                eprintln!("Failed to compile with gcc:\n{}", String::from_utf8_lossy(&out.stderr));
-                return;
-            }
+    match matches.subcommand() {
+        ("test", Some(test_matches)) => {
+            let FileNames { c_file, test_file, compiled_file } = get_files(&test_matches);
+
+            match compile(&c_file, &compiled_file) {
+                Ok(out) => {
+                    if !out.status.success() {
+                        eprintln!("Failed to compile with gcc:\n{}", String::from_utf8_lossy(&out.stderr));
+                        return;
+                    }
+                }
+                Err(e) => {
+                    eprintln!("Failed to execute compiler:\n{}", e);
+                    return;
+                }
+            };
+
+            let test_cases = match fs::read_to_string(&test_file) {
+                Ok(str) => str,
+                Err(e) => {
+                    eprintln!("Failed to read test file '{}': {}", test_file, e);
+                    return;
+                }
+            };
+
+            let lines = test_cases.lines();
+            let TestResult { passed, failed, total_time_ms } = execute_test_cases(&compiled_file, lines);
+
+            println!("{}", Blue.paint("------Summary------"));
+            let total = failed + passed;
+            println!("Tests: \n\t○ {} total\n\t{}\n\t{}",
+                     total,
+                     Red.blink().paint(format!("✗ {} failed", failed)),
+                     Green.paint(format!("✔ {} passed", passed)));
+            println!("Time elapsed: {} ms", total_time_ms);
         }
-        Err(e) => {
-            eprintln!("Failed to execute compiler:\n{}", e);
-            return;
-        }
-    };
-
-    let test_cases = match fs::read_to_string(&test_file) {
-        Ok(str) => str,
-        Err(e) => {
-            eprintln!("Failed to read test file '{}': {}", test_file, e);
-            return;
-        }
-    };
-
-    let lines = test_cases.lines();
-    let TestResult { passed, failed, total_time_ms } = execute_test_cases(&compiled_file, lines);
-
-    println!("{}", Blue.paint("------Summary------"));
-    let total = failed + passed;
-    println!("Tests: \n\t○ {} total\n\t{}\n\t{}",
-             total,
-             Red.blink().paint(format!("✗ {} failed", failed)),
-             Green.paint(format!("✔ {} passed", passed)));
-    println!("Time elapsed: {} ms", total_time_ms);
+        ("watch", Some(matches)) => {}
+        _ => panic!("Should be unreachable")
+    }
 }
 
